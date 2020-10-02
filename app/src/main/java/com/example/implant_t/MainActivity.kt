4b5a55650b18360455d0ad8e037bdee96e45fcac
DamirGDR <71948762+DamirGDR@users.noreply.github.com>
2020-10-02 11:30:38 +0300

    test

---
+++ app/src/main/java/com/example/implant_t/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.implant_t
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+
+
+}
